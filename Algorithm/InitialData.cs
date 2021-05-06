fbd6a89ad5c92ce13ac1031c4805ba65f5329060
Ulyan Yunakh <44743429+UlyanYunakh@users.noreply.github.com>
2021-05-06 17:02:28 +0300

    Algorithm (#1)
    * start classlib
    
    * gen algo
    
    * refactoring
    
    Co-authored-by: YanaGv <gv.yanina@gmail.com>

---
+++ Algorithm/InitialData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knapsack_Problem__cmd_
+{
+    public class InitialData
+    {
+        public List<List<Item>> Category { get; set; }
+    }
+}
---
+++ Algorithm/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knapsack_Problem__cmd_
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public int Cost { get; set; }
+        public int Worth { get; set; }
+    }
+}
